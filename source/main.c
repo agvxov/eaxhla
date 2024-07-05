c7aab1d9004132bf9090e9022f46c831d44af4b7
anon <anon@anon.anon>
2024-07-05 03:27:57 +0200

    WIP flex exit soft leak cleaning

--- source/main.c
+++ source/main.c
@@ -3,6 +3,8 @@
 #include "eaxhla.yy.h"
 #include "eaxhla.tab.h"
 
+extern void yyfree_leftovers(void);
+
 signed main(int argc, char * argv[]) {
     if (argc < 2) {
         printf("%s: <file>\n", argv[0]);
@@ -11,8 +13,11 @@ signed main(int argc, char * argv[]) {
     #if DEBUG == 1
         yydebug = 1;
     #endif
+
     yyin = fopen(argv[1], "r");
     yyparse();
+
+    yyfree_leftovers();
 
     return 0;
 }
