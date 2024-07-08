b804aca25789760b27b95e51ea1f39dcb1e37cc1
xolatile <xolatile@proton.me>
2024-07-08 08:46:17 -0400

    Added new testing files...

--- tool/test.asm
+++ tool/test.asm
@@ -1,190 +1,215 @@
 format ELF64 executable 3
 
 segment readable executable
+
 entry $
 
 	nop
 	nop
 	nop
 
+	fff:
+
+	nop
 	add rcx, r9
+	nop
 	add rcx, [x8]
+	nop
 	add rcx, 11223344h
+	nop
 	add [x8], rcx
+	nop
 	add qword[x8], 11223344h
+	nop
 	or  ecx, r9d
+	nop
 	or  ecx, [x4]
+	nop
 	or  ecx, 11223344h
+	nop
 	or  [x4], ecx
+	nop
 	or  dword[x4], 11223344h
+	nop
 	adc cx, r9w
+	nop
 	adc cx, [x2]
+	nop
 	adc cx, 1122h
+	nop
 	adc [x2], cx
+	nop
 	adc word[x2], 1122h
+	nop
 	sbb cl, r9b
+	nop
 	sbb cl, [x1]
+	nop
 	sbb cl, 11h
+	nop
 	sbb [x1], cl
+	nop
 	sbb byte[x1], 11h
-
-	;~add rcx  r9                      | ADD D64 REG R1 REG R9
-	;~4C 01 C9                         | 4C 01 C9
-	;~add rcx  [x8]                    | ADD D64 REG R1 MEM 0
-	;~48 03 0D E1 11 00 00             | 48 03 0D FF FF FF FF
-	;~add rcx  11223344h               | ADD D64 REG R1 IMM 0X11223344U
-	;~48 81 C1 44 33 22 11             | 48 81 C2 44 33 22 11
-	      ;~^^                                 ^^
-	;~add [x8] rcx                     | ADD D64 MEM 0  REG R9
-	;~48 01 0D D3 11 00 00             | 4C 01 0D FF FF FF FF
-	;~^^                                 ^^
-	;~add [x8] 11223344h               | ADD D64 MEM 0  IMM 0X11223344U
-	;~48 81 05 C8 11 00 00 44 33 22 11 | 48 81 05 FF FF FF FF 44 33 22 11
-
 	nop
+	and rcx, r9
 	nop
+	and rcx, [x8]
 	nop
-
-	xor rcx, r9
-	xor rcx, [x8]
-	xor rcx, 11223344h
-	xor [x8], rcx
-	xor qword[x8], 11223344h
-	xor ecx, r9d
-	xor ecx, [x4]
-	xor ecx, 11223344h
-	xor [x4], ecx
-	xor dword[x4], 11223344h
+	and rcx, 11223344h
+	nop
+	and [x8], rcx
+	nop
+	and qword[x8], 11223344h
+	nop
+	sub ecx, r9d
+	nop
+	sub ecx, [x4]
+	nop
+	sub ecx, 11223344h
+	nop
+	sub [x4], ecx
+	nop
+	sub dword[x4], 11223344h
+	nop
 	xor cx, r9w
+	nop
 	xor cx, [x2]
+	nop
 	xor cx, 1122h
+	nop
 	xor [x2], cx
+	nop
 	xor word[x2], 1122h
-	xor cl, r9b
-	xor cl, [x1]
-	xor cl, 11h
-	xor [x1], cl
-	xor byte[x1], 11h
-
 	nop
+	cmp cl, r9b
 	nop
+	cmp cl, [x1]
 	nop
-
-	xor rcx, r9
-	xor rcx, [x8]
-	xor rcx, 11223344h
-	xor [x8], rcx
-	xor qword[x8], 11223344h
-	xor ecx, r9d
-	xor ecx, [x4]
-	xor ecx, 11223344h
-	xor [x4], ecx
-	xor dword[x4], 11223344h
-	xor cx, r9w
-	xor cx, [x2]
-	xor cx, 1122h
-	xor [x2], cx
-	xor word[x2], 1122h
-	xor cl, r9b
-	xor cl, [x1]
-	xor cl, 11h
-	xor [x1], cl
-	xor byte[x1], 11h
-
+	cmp cl, 11h
 	nop
+	cmp [x1], cl
 	nop
+	cmp byte[x1], 11h
 	nop
-
 	inc rcx
+	nop
 	inc qword[x8]
+	nop
 	inc ecx
+	nop
 	inc dword[x4]
+	nop
 	inc cx
+	nop
 	inc word[x2]
+	nop
 	inc cl
+	nop
 	inc byte[x1]
-
 	nop
-	nop
-	nop
-
 	div r9
+	nop
 	div qword[x8]
+	nop
 	div r9d
+	nop
 	div dword[x4]
+	nop
 	div r9w
+	nop
 	div word[x2]
+	nop
 	div r9b
+	nop
 	div byte[x1]
-
 	nop
-	nop
-	nop
-
 	ret
+	nop
 	leave
+	nop
 	lock
+	nop
 	hlt
-
 	nop
-	nop
-	nop
-
 	sysenter
+	nop
 	sysexit
+	nop
 	syscall
+	nop
 	sysret
+	nop
 	pause
+	nop
 	cpuid
-
-	;~nop
-	;~nop
-	;~nop
-
-	;~jo fff
-	;~jno fff
-	;~jb fff
-	;~jae fff
-	;~je fff
-	;~jne fff
-	;~jbe fff
-	;~ja fff
-	;~js fff
-	;~jns fff
-	;~jpe fff
-	;~jpo fff
-	;~jl fff
-	;~jge fff
-	;~jle fff
-	;~jg fff
-
-	;~nop
-	;~nop
-	;~nop
-
-	;~cmovo rax, rcx
-	;~cmovno rax, rcx
-	;~cmovb rax, rcx
-	;~cmovae rax, rcx
-	;~cmove rax, rcx
-	;~cmovne rax, rcx
-	;~cmovbe rax, rcx
-	;~cmova rax, rcx
-	;~cmovs rax, rcx
-	;~cmovns rax, rcx
-	;~cmovpe rax, rcx
-	;~cmovpo rax, rcx
-	;~cmovl rax, rcx
-	;~cmovge rax, rcx
-	;~cmovle rax, rcx
-	;~cmovg rax, rcx
+	nop
+	jo fff
+	nop
+	jno fff
+	nop
+	jb fff
+	nop
+	jae fff
+	nop
+	je fff
+	nop
+	jne fff
+	nop
+	jbe fff
+	nop
+	ja fff
+	nop
+	js fff
+	nop
+	jns fff
+	nop
+	jpe fff
+	nop
+	jpo fff
+	nop
+	jl fff
+	nop
+	jge fff
+	nop
+	jle fff
+	nop
+	jg fff
+	nop
+	cmovo rax, rcx
+	nop
+	cmovno rax, rcx
+	nop
+	cmovb rax, rcx
+	nop
+	cmovae rax, rcx
+	nop
+	cmove rax, rcx
+	nop
+	cmovne rax, rcx
+	nop
+	cmovbe rax, rcx
+	nop
+	cmova rax, rcx
+	nop
+	cmovs rax, rcx
+	nop
+	cmovns rax, rcx
+	nop
+	cmovpe rax, rcx
+	nop
+	cmovpo rax, rcx
+	nop
+	cmovl rax, rcx
+	nop
+	cmovge rax, rcx
+	nop
+	cmovle rax, rcx
+	nop
+	cmovg rax, rcx
+	;~;mov
+	;~;jmp
 
 	nop
-	nop
-	nop
-
-	;mov
-	;jmp
 
 	nop
 	nop
