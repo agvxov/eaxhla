353768456559a9b84b08c0dd03b80a9a3bdf20cc
anon <anon@anon.anon>
2024-07-09 15:11:33 +0200

    blew tcl to multiple scripts

---
+++ debug/scanner_instructions.tcl
@@ -0,0 +1,15 @@
+source debug/instructions.tcl
+
+proc make_scanner_instructions {is} {
+    proc make_scanner_instruction {i} {
+        puts [format "%s    { return I%s; }" \
+                    [lindex $i 0] \
+                    [string toupper [lindex $i 0]]
+            ]
+    }
+    foreach i $is {
+        make_scanner_instruction $i
+    }
+}
+
+make_scanner_instructions $instructions
