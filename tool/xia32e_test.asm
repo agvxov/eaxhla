59da44580a72481d1f2e0daee45807eaf1d01104
xolatile <xolatile@proton.me>
2024-07-16 08:00:45 -0400

    Added very expanded example...

--- tool/xia32e_test.asm
+++ tool/xia32e_test.asm
@@ -3,8 +3,15 @@ nop mov edi 1
 nop mov esi heyo
 nop mov edx 5
 nop syscall
+nop mov eax 1
+nop mov edi 1
+nop mov esi cyaa
+nop mov edx 5
+nop syscall
 nop mov eax 60
 nop mov edi 60
 nop syscall
-nop . heyo db 5 72 69 89 79 10
+nop
+. heyo db 5 72 69 89 79 10
+. cyaa db 5 67 89 65 65 10
 end
