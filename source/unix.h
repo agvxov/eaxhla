95e73027ee3c159ee7f74075468635e420d77149
xolatile <xolatile@proton.me>
2024-07-18 17:25:38 -0400

    Refactored UNIX...

--- source/unix.h
+++ source/unix.h
@@ -1,8 +1,5 @@
 #ifndef UNIX_H
 
-#include <stdint.h>
-#include <string.h>
-
 #define ELF_MAIN_HEADER_SIZE (0x40)
 #define ELF_TEXT_SECTOR_SIZE (0x38)
 #define ELF_DATA_SECTOR_SIZE (0x38)
@@ -11,19 +8,16 @@
                        + ELF_TEXT_SECTOR_SIZE \
                        + ELF_DATA_SECTOR_SIZE)
 
-extern uint8_t elf_main_header_byte [ELF_MAIN_HEADER_SIZE];
-extern uint8_t elf_text_sector_byte [ELF_TEXT_SECTOR_SIZE];
-extern uint8_t elf_data_sector_byte [ELF_DATA_SECTOR_SIZE];
+extern unsigned char elf_main_header_byte[ELF_MAIN_HEADER_SIZE];
+extern unsigned char elf_text_sector_byte[ELF_TEXT_SECTOR_SIZE];
+extern unsigned char elf_data_sector_byte[ELF_DATA_SECTOR_SIZE];
 
-extern void elf_main_header (uint8_t  has_program,
-                             uint8_t  for_linux,
-                             uint8_t  for_x86_64);
+extern void elf_main_header(unsigned char has_program,
+                            unsigned char for_linux,
+                            unsigned char for_x86_64);
 
-extern void elf_text_sector (uint64_t text_size,
-                             uint64_t data_size);
-
-extern void elf_data_sector (uint64_t text_size,
-                             uint64_t data_size);
+extern void elf_text_sector(unsigned long text_size, unsigned long data_size);
+extern void elf_data_sector(unsigned long text_size, unsigned long data_size);
 
 #define UNIX_H
 #endif
