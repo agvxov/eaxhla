040b89d92db39091d8de40f92080e2eb37fd9204
xolatile <xolatile@proton.me>
2024-07-18 16:59:01 -0400

    splint -weak +ignoresigns source/assembler.c

--- source/assembler.h
+++ source/assembler.h
@@ -1,36 +1,22 @@
 #ifndef ASSEMBLER_H
 
-typedef signed   int  form;
-typedef unsigned int  next;
-typedef unsigned char byte;
+enum {
+	D8,               D16,              D32,              D64
+};
 
-typedef enum {
-	D8  = 0,
-	D16 = 1,
-	D32 = 2,
-	D64 = 3,
-} size_index;
+enum {
+	REL,              REG,              MEM,              IMM
+};
 
-typedef enum {
-	REL,              REG,              MEM,              IMM,
-} type_index;
-
-typedef enum {
-	ASMDIRMEM, // Assembler directive: Insert label location.
-	ASMDIRREL, // Assembler directive: Insert relative location.
-	ASMDIRIMM, // Assembler directive: Append immediate.
-	ASMDIRREP, // Assembler directive: --
-	//
+enum {
+	ASMDIRMEM,        ASMDIRREL,        ASMDIRIMM,        ASMDIRREP,
 	ADD,              OR,               ADC,              SBB,
 	AND,              SUB,              XOR,              CMP,
-	//
 	INC,              DEC,              NOT,              NEG,
 	MUL,              IMUL,             DIV,              IDIV,
-	//
 	NOP,              RETN,             RETF,             LEAVE,
 	LOCK,             HLT,              POPF,             PUSHF,
 	WAIT,
-	//
 	SYSENTER,         SYSEXIT,          SYSCALL,          SYSRET,
 	PAUSE,            CPUID,            EMMS,             RSM,
 	FNOP,             FCHS,             FABS,             FTST,
@@ -40,21 +26,17 @@ typedef enum {
 	FXTRACT,          FPREM1,           FDECSTP,          FINCSTP,
 	FPREM,            FYL2XP1,          FSQRT,            FSINCOS,
 	FRNDINT,          FSCALE,           FSIN,             FCOS,
-	//
 	ENTER,            CALL,             IN,               OUT,
-	//
 	JMP,
 	JO,               JNO,              JB,               JAE,
 	JE,               JNE,              JBE,              JA,
 	JS,               JNS,              JPE,              JPO,
 	JL,               JGE,              JLE,              JG,
-	//
 	MOV,
 	CMOVO,            CMOVNO,           CMOVB,            CMOVAE,
 	CMOVE,            CMOVNE,           CMOVBE,           CMOVA,
 	CMOVS,            CMOVNS,           CMOVPE,           CMOVPO,
 	CMOVL,            CMOVGE,           CMOVLE,           CMOVG,
-	//
 	PUSH,             POP,              BSWAP,            TEST,
 	RCL,              RCR,              ROL,              ROR,
 	SHL,              SHR,              SAL,              SAR,
@@ -62,25 +44,24 @@ typedef enum {
 	LOOP,             LOOPE,            LOOPNE,           MOVBE,
 	XADD,             XCHG,             LEA,              POPCNT,
 	INTI,             BSF,              BSR,              BOUND,
-	//
 	FADD,             FMUL,             FCOM,             FCOMP,
-	FSUB,             FSUBR,            FDIV,             FDIVR,
-} operation_index;
+	FSUB,             FSUBR,            FDIV,             FDIVR
+};
 
-typedef enum {
+enum {
 	R0,               R1,               R2,               R3,
 	R4,               R5,               R6,               R7,
 	R8,               R9,               R10,              R11,
-	R12,              R13,              R14,              R15,
-} operand_index;
+	R12,              R13,              R14,              R15
+};
 
-extern next   text_sector_size;
-extern byte * text_sector_byte;
+extern unsigned int    text_entry_point;
+extern unsigned int    text_sector_size;
+extern unsigned char * text_sector_byte;
 
-extern          int was_instruction_array_empty;
-extern unsigned int text_entry_point;
+extern int was_instruction_array_empty;
 
-extern void assemble (next count, next * array);
+extern void assemble (unsigned int count, unsigned int * array);
 
 #define ASSEMBLER_H
 #endif
